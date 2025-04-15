45f7a1a6c2997b188643d05499797ec591d6df98
VikaSukhara <125195456+VikaSukhara@users.noreply.github.com>
2025-04-15 17:39:45 +0000

    Deploying to gh-pages from @ VikaSukhara/cars@e28b86204f9ef07d1f90de2e4530d94b9da53ccc 🚀

---
+++ static/js/124.8d890d69.chunk.js
@@ -0,0 +1,315 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[124],{124:(e,t,i)=>{i.r(t),i.d(t,{default:()=>x});var n=i(470),r=i(43),o=i(3),l=i(841),s=i(717),a=i(464);const c=a.Ay.div`
+  padding-left: 128px;
+  padding-right: 128px;
+`,d=a.Ay.h2`
+  color: #121417;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  margin-top: 250px;
+  text-align: center;
+`;var p=i(579);const x=()=>{const e=(0,o.d4)(s.e9),t=(0,o.d4)(s.jH),i=(0,o.wA)();return(0,r.useEffect)((()=>{i((0,l.q)(e))}),[i,e]),(0,p.jsx)("div",{children:(0,p.jsx)(c,{children:(0,p.jsx)("div",{style:{marginTop:"127px"},children:t.length>0?(0,p.jsx)(n.o,{catalogCars:t}):(0,p.jsx)(d,{children:"You don't have any favorite cars"})})})})}},470:(e,t,i)=>{i.d(t,{o:()=>W});var n,r=i(43),o=i(464);function l(){return l=Object.assign?Object.assign.bind():function(e){for(var t=1;t<arguments.length;t++){var i=arguments[t];for(var n in i)({}).hasOwnProperty.call(i,n)&&(e[n]=i[n])}return e},l.apply(null,arguments)}function s(e,t){let{title:i,titleId:o,...s}=e;return r.createElement("svg",l({width:18,height:18,viewBox:"0 0 18 18",fill:"none",xmlns:"http://www.w3.org/2000/svg",ref:t,"aria-labelledby":o},s),i?r.createElement("title",{id:o},i):null,n||(n=r.createElement("path",{d:"M15.63 3.4575C15.247 3.07425 14.7921 2.77023 14.2915 2.56281C13.7909 2.35539 13.2544 2.24863 12.7125 2.24863C12.1707 2.24863 11.6341 2.35539 11.1335 2.56281C10.6329 2.77023 10.1781 3.07425 9.79503 3.4575L9.00003 4.2525L8.20503 3.4575C7.43126 2.68373 6.3818 2.24903 5.28753 2.24903C4.19325 2.24903 3.1438 2.68373 2.37003 3.4575C1.59626 4.23127 1.16156 5.28072 1.16156 6.375C1.16156 7.46927 1.59626 8.51873 2.37003 9.2925L3.16503 10.0875L9.00003 15.9225L14.835 10.0875L15.63 9.2925C16.0133 8.90943 16.3173 8.45461 16.5247 7.95401C16.7321 7.45342 16.8389 6.91686 16.8389 6.375C16.8389 5.83313 16.7321 5.29657 16.5247 4.79598C16.3173 4.29539 16.0133 3.84056 15.63 3.4575Z",stroke:"white",strokeOpacity:.8,strokeWidth:1.5,strokeLinecap:"round",strokeLinejoin:"round"})))}const a=r.forwardRef(s),c=(i.p,(0,o.Ay)(a)`
+  width: 18px;
+  height: 18px;
+
+  color: white;
+  width: 18px;
+  height: 18px;
+  
+  transition: fill scale 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    scale: 1.1;
+  }
+  &:focus {
+    fill: green;
+    color: green;
+  }
+`),d=o.Ay.button`
+  position: absolute;
+  top: 14px;
+  right: 14px;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  background-color: transparent;
+`,p=(o.Ay.div`
+  padding-left: 128px;
+  padding-right: 128px;
+`,o.Ay.ul`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  column-gap: 29px;
+  row-gap: 50px;
+`),x=o.Ay.li`
+  width: 247px;
+  height: 426px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 24px;
+  position: relative;
+`,h=o.Ay.img`
+  width: 247px;
+  height: 268px;
+  border-radius: 24px;
+  margin-bottom: 14px;
+`,f=o.Ay.p`
+  color: #121417;
+  font-family: Manrope;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px; /* 150% */
+  margin-bottom: 8px;
+`,g=o.Ay.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`,m=o.Ay.div`
+  display: flex;
+  flex-direction: row;
+  gap: 3px;
+`,y=o.Ay.p`
+  color: rgba(18, 20, 23, 0.5);
+  font-family: Manrope;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px;
+  padding-left: 6px;
+  margin-top: 0;
+  margin-bottom: 0;
+`,u=o.Ay.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin-bottom: 28px;
+`,b=o.Ay.button`
+  display: flex;
+  height: 44px;
+  font-size: 18px;
+
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  background: green;
+  color: #ffffff;
+  border: none;
+  
+  &:hover {
+    background:rgb(11, 90, 39); 
+  }
+`,j=o.Ay.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.8);
+  opacity: 1;
+  transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1),
+    visibility 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`,w=o.Ay.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) scale(1);
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  max-width: 541px;
+  overflow-y: auto;
+  border-radius: 10px;
+  background-color: white;
+  width: 541px;
+`,v=o.Ay.div`
+  padding: 40px 36px 0 35px;
+`,A=o.Ay.button`
+  width: 35px;
+  height: 35px;
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  background: none;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  color: black;
+  transition: transform 250ms cubic-bezier(0.4, 0, 0.2, 1);
+`,k=(o.Ay.ul`
+  margin-left: 128px;
+  margin-right: 128px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 29px;
+`,o.Ay.li`
+  width: 247px;
+  height: 426px;
+  display: flex;
+  flex-direction: column;
+`,o.Ay.img`
+  width: 469px;
+  height: 255px;
+  border-radius: 24px;
+`),C=o.Ay.img``,z=o.Ay.p`
+  color: #121417;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px;
+`,L=o.Ay.h2`
+  color: #121417;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px; /* 142.857% */
+  margin-bottom: 8px;
+`,E=o.Ay.h3`
+  color: #121417;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 20px; /* 142.857% */
+  margin-bottom: 8px;
+`,M=o.Ay.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`,I=o.Ay.div`
+  display: flex;
+  flex-direction: row;
+  gap: 3px;
+  margin-top: 8px;
+  margin-bottom: 8px;
+`,P=o.Ay.p`
+  color: rgba(18, 20, 23, 0.5);
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px;
+`,S=o.Ay.span`
+  color: #d8d6d4;
+  margin: 0 5px;
+`,_=o.Ay.div`
+  width: 350px;
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin-bottom: 20px;
+`,O=o.Ay.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  margin-bottom: 24px;
+  gap: 6px;
+`,T=(o.Ay.p`
+  color: #121417;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  margin-top: 14px;
+  margin-bottom: 24px;
+`,o.Ay.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+`),R=o.Ay.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3px;
+  border-radius: 35px;
+  background: #f9f9f9;
+
+  display: flex;
+  padding: 7px 14px;
+  justify-content: center;
+  align-items: center;
+  padding: 7px 14px;
+
+  color: #363535;
+
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 18px; /* 150% */
+  letter-spacing: -0.24px;
+  margin-bottom: 8px;
+`,Y=o.Ay.span`
+  color: green;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: 18px;
+  letter-spacing: -0.24px;
+`,$=o.Ay.button`
+  display: inline-flex;
+
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  background: green;
+  color: #ffffff;
+  width: 168px;
+  height: 44px;
+  border: none;
+  margin-top: 16px;
+  margin-bottom: 40px;
+  &:hover {
+    background: rgb(11, 90, 39);
+  }
+`,q=o.Ay.a`
+  color: #ffffff;
+`;const H=i.p+"static/media/cross.f3cbdd58dc25ca7cbb3d242106b56fb9.svg";var N=i(579);const B=e=>{let{car:t,close:i}=e;(0,r.useEffect)((()=>{const e=e=>{"Escape"===e.code&&i()};return document.body.style.overflow="hidden",document.addEventListener("keydown",e),()=>{document.removeEventListener("keydown",e),document.body.style.overflow="auto"}}),[i]);return(0,N.jsx)(j,{onClick:e=>{e.target===e.currentTarget&&i()},children:(0,N.jsxs)(w,{children:[(0,N.jsx)(A,{children:(0,N.jsx)(C,{src:H,alt:"",onClick:()=>i()})}),(0,N.jsxs)(v,{children:[(0,N.jsx)(k,{src:t.img,alt:"img auto"}),(0,N.jsx)(M,{children:(0,N.jsxs)(I,{children:[(0,N.jsxs)(z,{children:[t.make,","]}),(0,N.jsx)(z,{children:t.year})]})}),(0,N.jsxs)(_,{children:[(0,N.jsxs)(P,{children:[t.address.slice(20)," ",(0,N.jsx)(S,{children:"|"})]}),(0,N.jsxs)(P,{children:["Id: ",t.id," ",(0,N.jsx)(S,{children:"|"})]}),(0,N.jsxs)(P,{children:["Year: ",t.year," ",(0,N.jsx)(S,{children:"|"})]}),(0,N.jsxs)(P,{children:["Type: ",t.type," "]}),(0,N.jsxs)(P,{children:["FuelConsumption: ",t.fuelConsumption," ",(0,N.jsx)(S,{children:"|"})," "]}),(0,N.jsxs)(P,{children:["EngineSize: ",t.engineSize," ",(0,N.jsx)(S,{children:"|"})]})]}),(0,N.jsx)(E,{children:"Accessories and functionalities:"}),(0,N.jsxs)(O,{children:[t.accessories.map(((e,t)=>(0,N.jsxs)(P,{children:[e," ",(0,N.jsx)(S,{children:"|"})]},`accessory-${t}`))),t.functionalities.map(((e,t)=>(0,N.jsxs)(P,{children:[e," ",(0,N.jsx)(S,{children:"|"})]},`functionality-${t}`)))]}),(0,N.jsx)(L,{children:"Rental Conditions:"}),(0,N.jsxs)(T,{children:[(0,N.jsxs)(R,{children:["Minimum age : ",(0,N.jsx)(Y,{children:"25"})]}),(0,N.jsx)(R,{children:"Valid driver\u2019s license"}),(0,N.jsx)(R,{children:"Security deposite required"}),(0,N.jsxs)(R,{children:["Mileage: ",(0,N.jsx)(Y,{children:"5,858"})]}),(0,N.jsxs)(R,{children:["Price: ",(0,N.jsx)(Y,{children:t.rentalPrice})]})]}),(0,N.jsx)($,{type:"buttom",children:(0,N.jsx)(q,{href:"tel:+380730000000",onClick:()=>i(),children:"Rental Car"})})]})]})})};var D=i(635),F=i(3),U=i(717),V=i(460);const W=e=>{let{catalogCars:t}=e;const i=(0,F.wA)(),[n,o]=(0,r.useState)(!1),[l,s]=(0,r.useState)(null),a=(0,F.d4)(U.e9),j=e=>{const t=+e.currentTarget.id,n=a.findIndex((e=>e===t));i(-1!==n?(0,V.Ym)(t):(0,V.C4)(t))},w=e=>{const t=e.split(",");return t.length>=2?t[t.length-2].trim():""};return(0,N.jsxs)("div",{children:[(0,N.jsx)(D.mc,{children:(0,N.jsx)(p,{children:t.map((e=>{const t=a.includes(e.id);return(0,N.jsxs)(x,{children:[(0,N.jsx)(h,{src:e.img,alt:"img auto"}),(0,N.jsx)(d,{id:e.id,onClick:j,children:(0,N.jsx)(c,{fill:t?"green":"none"})}),(0,N.jsxs)(g,{children:[(0,N.jsxs)(m,{children:[(0,N.jsxs)(f,{children:[e.make,", "]}),(0,N.jsxs)(f,{style:{marginLeft:"2px"},children:[" ",e.year]})]}),(0,N.jsx)(f,{children:e.rentalPrice})]}),(0,N.jsxs)(u,{children:[(0,N.jsxs)(y,{children:[w(e.address)," |"]}),(0,N.jsx)(y,{children:"Ukraine |"}),(0,N.jsxs)(y,{children:[e.rentalCompany," |"]}),(0,N.jsxs)(y,{children:[e.type," "]}),(0,N.jsxs)(y,{children:[e.model," |"]}),(0,N.jsxs)(y,{children:[e.id," |"]}),(0,N.jsxs)(y,{children:[e.accessories[0]," "]})]}),(0,N.jsx)(b,{type:"button",onClick:()=>(e=>{o(!0),s(e)})(e),children:"Learn more"})]},e.id)}))})}),n&&(0,N.jsx)(B,{car:l,close:()=>{o(!1),s(null)}})]})}},635:(e,t,i)=>{i.d(t,{$n:()=>c,Eo:()=>l,Ih:()=>a,hE:()=>s,mc:()=>o});var n=i(475),r=i(464);const o=r.Ay.div`
+  margin-bottom: 50px;
+  width: 1184px;
+  margin: 50px auto;
+`,l=r.Ay.div`
+  margin-top: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-right: auto;
+  margin-left: auto;
+  text-align: center;
+`,s=r.Ay.h1`
+  color: #121417;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 133.333% */
+  margin-bottom: 50px;
+`,a=r.Ay.p`
+  color: #121417;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+  margin-bottom: 30px;
+`,c=(0,r.Ay)(n.N_)`
+  color: #fbfbfb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  width: 350px;
+  height: 50px;
+  border: 1px solid rgba(251, 251, 251, 0.4);
+  background: green;
+  margin: 30px auto;
+  padding: 10px;
+
+  transition: var(--button-transition);
+
+  &: hover {
+    background: rgb(11, 90, 39);
+    transform: scale(var(--button-scale));
+  }
+
+  color: rgb(255, 255, 255);
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+`},717:(e,t,i)=>{i.d(t,{Nz:()=>r,P7:()=>s,e9:()=>l,jH:()=>o});var n=i(99);const r=e=>e.catalog.isLoading,o=e=>e.favorite.favorite,l=e=>e.favorite.favouriteIDs,s=(0,n.Mz)([e=>e.catalog.items,e=>e.filters],((e,t)=>e.filter((e=>e.make.toLowerCase().includes(t.model.toLowerCase())))))}}]);
+//# sourceMappingURL=124.8d890d69.chunk.js.map
---
+++ static/js/264.2629702e.chunk.js
@@ -0,0 +1,53 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[264],{264:(e,t,n)=>{n.r(t),n.d(t,{default:()=>i});var o=n(635),r=n(579);const i=()=>(0,r.jsx)(o.mc,{children:(0,r.jsxs)(o.Eo,{children:[(0,r.jsx)(o.hE,{children:"The best rental car company in Ukraine"}),(0,r.jsx)(o.Ih,{children:"We offer machines of different brands and different price categories"}),(0,r.jsx)(o.Ih,{children:"Go to the categories and choose your car"}),(0,r.jsx)(o.$n,{to:"/catalog",children:"Choose the car"})]})})},635:(e,t,n)=>{n.d(t,{$n:()=>h,Eo:()=>a,Ih:()=>s,hE:()=>c,mc:()=>i});var o=n(475),r=n(464);const i=r.Ay.div`
+  margin-bottom: 50px;
+  width: 1184px;
+  margin: 50px auto;
+`,a=r.Ay.div`
+  margin-top: 200px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-right: auto;
+  margin-left: auto;
+  text-align: center;
+`,c=r.Ay.h1`
+  color: #121417;
+  font-size: 40px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px; /* 133.333% */
+  margin-bottom: 50px;
+`,s=r.Ay.p`
+  color: #121417;
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px; /* 142.857% */
+  margin-bottom: 30px;
+`,h=(0,r.Ay)(o.N_)`
+  color: #fbfbfb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 30px;
+  width: 350px;
+  height: 50px;
+  border: 1px solid rgba(251, 251, 251, 0.4);
+  background: green;
+  margin: 30px auto;
+  padding: 10px;
+
+  transition: var(--button-transition);
+
+  &: hover {
+    background: rgb(11, 90, 39);
+    transform: scale(var(--button-scale));
+  }
+
+  color: rgb(255, 255, 255);
+  font-size: 35px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 24px;
+`}}]);
+//# sourceMappingURL=264.2629702e.chunk.js.map
